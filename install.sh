2cbdafc357200871499d8365ec57df7703d3e0af
Mateus-Lacerda <mateuslacerda253@gmail.com>
2025-05-05 18:06:40 -0300

    feat(install): enhance installation script with new tools and configs
    - Added `btop` and `cmatrix` to the base installation packages.
    - Consolidated multiple `pacman` commands into a single line for brevity.
    - Replaced `yay` commands with `git clone` for zsh plugins and themes.
    - Updated GRUB theme installation with manual editing prompt.
    - Added steps to clone and configure dotfiles, Neovim, and zshrc.
    - Introduced Rofi theme installation with backup of existing config.

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ rm -rf yay
 
 # Base stuff
 sudo pacman -S --noconfirm git man mupdf neofetch wget curl \
-    pavucontrol bluez blueman arandr kitty
+    pavucontrol bluez blueman arandr kitty btop cmatrix
 
 # Cool fonts
 yay -S --noconfirm nerd-fonts-sf-mono
@@ -25,14 +25,7 @@ yay -S --noconfirm i3lock-color
 sudo pacman -S --noconfirm neovim
 
 # Neovim stuff
-# Luarocks
-sudo pacman -S --noconfirm luarocks
-# fd
-sudo pacman -S --noconfirm fd
-# ripgrep
-sudo pacman -S --noconfirm ripgrep
-# xclip
-sudo pacman -S --noconfirm xclip
+sudo pacman -S --noconfirm luarocks fd ripgrep xclip
 
 # Install zsh
 sudo pacman -S --noconfirm zsh
@@ -42,13 +35,12 @@ sudo chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install ohmyzsh plugins
-# powerlevel10k, zsh-autosuggestions, zsh-syntax-highlighting
 # zsh-autosuggestions
-yay -S --noconfirm zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # zsh-syntax-highlighting
-yay -S --noconfirm zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 # powerlevel10k
-yay -S --noconfirm zsh-theme-powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
 # Install tmux
 sudo pacman -S --noconfirm tmux
@@ -56,21 +48,35 @@ sudo pacman -S --noconfirm tmux
 # grub theme
 git clone https://github.com/mino29/tokyo-night-grub.git
 cd tokyo-night-grub
-sudo cp -r themes/tokyo-night /boot/grub/themes
-sudo sed -i 's/GRUB_THEME=.*/GRUB_THEME="/boot/grub/themes/tokyo-night/theme.txt"/' /etc/default/grub
+sudo cp -r tokyo-night /boot/grub/themes
+echo 'Copy this: GRUB_THEME="/boot/grub/themes/tokyo-night/theme.txt" to /etc/default/grub, press enter to continue'; read
+sudo nvim /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
+mkdir Downloads
+cd Downloads
 # Get my dotfiles
-git clone git@github.com:Mateus-Lacerda/.config.git ./configs
+git clone https://github.com/Mateus-Lacerda/.config.git
 # Get my neovim config
-git clone git@github.com:Mateus-Lacerda/neovim_config ./configs/.config/nvim
+git clone https://github.com/Mateus-Lacerda/neovim_config.git
+# Get my zsh config
+git clone https://github.com/Mateus-Lacerda/zshrc.git
 
-sudo cp -r ./configs/.config ~/
+sudo cp -rf ./.config/* ~/.config/
+sudo mkdir -p ~/.config/nvim
+sudo cp -rf ./neovim_config/* ~/.config/nvim/
+sudo cp -rf ./zshrc/* ~/
 
+# Rofi theme
+mv ~/.config/rofi ~/.config/rofi.bak
+git clone https://github.com/w8ste/Tokyonight-rofi-theme.git ~/.config/rofi
+sudo mv ~/.config/rofi/tokyonight.rasi /usr/share/rofi/themes
+sudo mv ~/.config/rofi/tokyonight_big1.rasi /usr/share/rofi/themes
+sudo mv ~/.config/rofi/tokyonight_big2.rasi /usr/share/rofi/themes
+rm ~/.config/rofi/README.md
 
 
 echo "Press Enter to reboot"; read
 
 # Reboot
 sudo reboot
-
