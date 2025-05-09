8e781ccc537954ac2d25265a4f4dc003accf43ce
Mateus-Lacerda <mlacerdam.ai@gmail.com>
2025-05-09 19:35:19 -0300

    feat: migrate from kitty to ghostty terminal
    Replaced all references to kitty with ghostty in the install script and
    .gitignore. Added a basic ghostty config with Fira Code font and
    tokyonight theme. Removed the kitty configuration file. Updated package
    installation to use ghostty instead of kitty. Ensured all related
    commands and directories reflect the new terminal emulator.

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@ check_error() {
 # Verifica se o sistema está online
 check_internet() {
     msg "Verificando conexão com a internet..."
-    ping -c 1 archlinux.org > /dev/null 2>&1 || error "Sem conexão com a internet"
+    ping -c 1 archlinux.org > /dev/null 2>&1
 }
 
 # Função para instalar pacotes com pacman
@@ -62,7 +62,7 @@ backup_dir() {
 
 # Verifica pré-requisitos
 msg "Verificando se o sistema é Arch Linux..."
-[ -f /etc/arch-release ] || error "Este script deve ser executado no Arch Linux"
+[ -f /etc/arch-release ]
 check_internet
 
 # Atualiza o sistema
@@ -74,7 +74,7 @@ check_error "Falha ao atualizar o sistema"
 if ! command -v yay &> /dev/null; then
     msg "Instalando yay..."
     install_pacman base-devel git
-    git clone https://aur.archlinux.org/yay.git || error "Falha ao clonar yay"
+    git clone https://aur.archlinux.org/yay.git
     cd yay
     makepkg -si
     check_error "Falha ao instalar yay"
@@ -84,7 +84,9 @@ fi
 
 # Instala pacotes base
 install_pacman base-devel git man mupdf neofetch wget curl \
-    pavucontrol bluez blueman arandr kitty btop cmatrix
+    pavucontrol bluez blueman arandr ghostty btop cmatrix
+
+install_yay zen-browser-bin
 
 # Instala fontes
 install_pacman ttf-fira-code
@@ -114,53 +116,53 @@ fi
 
 # Instala plugins do oh-my-zsh
 msg "Instalando plugins do oh-my-zsh..."
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions || error "Falha ao clonar zsh-autosuggestions"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git || error "Falha ao clonar zsh-syntax-highlighting"
-sudo mv zsh-syntax-highlighting /usr/share/ || error "Falha ao mover zsh-syntax-highlighting"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" || error "Falha ao clonar powerlevel10k"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+sudo mv zsh-syntax-highlighting /usr/share/
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
 # Instala tmux
 install_pacman tmux
 
 # Configura tema do GRUB
 msg "Configurando tema Tokyo Night para o GRUB..."
-git clone https://github.com/mino29/tokyo-night-grub.git || error "Falha ao clonar tema do GRUB"
+git clone https://github.com/mino29/tokyo-night-grub.git
 cd tokyo-night-grub
-sudo cp -r tokyo-night /boot/grub/themes || error "Falha ao copiar tema do GRUB"
+sudo cp -r tokyo-night /boot/grub/themes
 cd ..
 [ -d tokyo-night-grub ] && rm -rf tokyo-night-grub
 msg "Adicionando tema ao /etc/default/grub..."
 sudo sed -i '/^GRUB_THEME=/d' /etc/default/grub
 echo 'GRUB_THEME="/boot/grub/themes/tokyo-night/theme.txt"' | sudo tee -a /etc/default/grub
-sudo grub-mkconfig -o /boot/grub/grub.cfg || error "Falha ao atualizar configuração do GRUB"
+sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 # Aplica dotfiles
 msg "Aplicando dotfiles..."
-git clone https://github.com/Mateus-Lacerda/.config.git || error "Falha ao clonar dotfiles"
+git clone https://github.com/Mateus-Lacerda/.config.git
 backup_dir ~/.config
-cp -rf .config/* ~/.config/ || error "Falha ao copiar dotfiles"
+cp -rf .config/* ~/.config/
 [ -d .config ] && rm -rf .config
 
 # Configura neovim
 msg "Aplicando configuração do neovim..."
-git clone https://github.com/Mateus-Lacerda/neovim_config.git || error "Falha ao clonar configuração do neovim"
+git clone https://github.com/Mateus-Lacerda/neovim_config.git
 backup_dir ~/.config/nvim
-mv neovim_config ~/.config/nvim || error "Falha ao mover configuração do neovim"
+mv neovim_config ~/.config/nvim
 
 # Configura zsh
 msg "Aplicando configuração do zsh..."
-git clone https://github.com/Mateus-Lacerda/zshrc.git || error "Falha ao clonar configuração do zsh"
+git clone https://github.com/Mateus-Lacerda/zshrc.git
 backup_dir ~/.zshrc
-mv zshrc/* ~ || error "Falha ao mover configuração do zsh"
+mv zshrc/* ~
 [ -d zshrc ] && rm -rf zshrc
 
 # Configura tema do rofi
 msg "Aplicando tema Tokyo Night para o rofi..."
 backup_dir ~/.config/rofi
-git clone https://github.com/w8ste/Tokyonight-rofi-theme.git ~/.config/rofi || error "Falha ao clonar tema do rofi"
-sudo mv ~/.config/rofi/tokyonight.rasi /usr/share/rofi/themes || error "Falha ao mover tokyonight.rasi"
-sudo mv ~/.config/rofi/tokyonight_big1.rasi /usr/share/rofi/themes || error "Falha ao mover tokyonight_big1.rasi"
-sudo mv ~/.config/rofi/tokyonight_big2.rasi /usr/share/rofi/themes || error "Falha ao mover tokyonight_big2.rasi"
+git clone https://github.com/w8ste/Tokyonight-rofi-theme.git ~/.config/rofi
+sudo mv ~/.config/rofi/tokyonight.rasi /usr/share/rofi/themes
+sudo mv ~/.config/rofi/tokyonight_big1.rasi /usr/share/rofi/themes
+sudo mv ~/.config/rofi/tokyonight_big2.rasi /usr/share/rofi/themes
 [ -f ~/.config/rofi/README.md ] && rm ~/.config/rofi/README.md
 
 # Finalização
